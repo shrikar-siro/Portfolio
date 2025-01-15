dc012051141b8b89e834e9050e94e56e63630705
shrikar-siro <siro.shrikar@gmail.com>
2025-01-15 17:47:36 -0500

    reformatted section description

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -7,11 +7,9 @@ function About() {
         <div className="title">About Me</div>
         <div className="description">
           Hey! My name is Shrikar Sirobhushanam, and I'm currently a 2nd year at
-          UVA majoring in Computer Science. Currently, I work as an
-          undergraduate research assistant at the UVA School of Data Science,
-          exploring AI-powered solutions in the neuroscience space. Beyond
-          academics, I enjoy travelling, playing tennis & chess, and watching
-          F1. Get to know more abouut me by exploring the sections below!
+          UVA majoring in Computer Science. Beyond academics, I enjoy
+          travelling, playing tennis & chess, and watching F1. Get to know more
+          abouut me by exploring the sections below!
         </div>
       </div>
     </>
