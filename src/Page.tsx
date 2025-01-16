9f6be076bdf8e4c1ef23ec91ade9b8b90090de14
shrikar-siro <siro.shrikar@gmail.com>
2025-01-15 22:39:11 -0500

    removing some warnings

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,11 +1,11 @@
 import About from "./Components/About";
-import Contact from "./Components/Contact";
+//import Contact from "./Components/Contact";
 import Experience from "./Components/Experience";
 import Navbar from "./Components/Navbar";
-import Projects from "./Components/Projects";
+//import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 import "./Styling/Page.css";
-import { Route, Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
 function Page() {
   return (
